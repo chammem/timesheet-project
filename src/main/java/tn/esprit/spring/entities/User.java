d17ed4d7632511b41b085ed82a9b3120a45a2971
chammem sinda <chammemcinda@gmail.com>
2025-02-24 16:26:01 +0100

    Chap6-Unit tests

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -69,12 +69,17 @@ public class User implements Serializable {
 	public void setId(Long id) {
 		this.id = id;
 	}
-
+	public String getFirstName() {
+		return lastName;
+	}
 	public String getLastName() {
 		return lastName;
 	}
 	public void setLastName(String lName) {
 		this.lastName = lName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
 	}
 	public Date getDateNaissance() {
 		return dateNaissance;
